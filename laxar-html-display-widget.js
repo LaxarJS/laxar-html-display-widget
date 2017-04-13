db8744eb3649de2359c12d28f170780c0eaebef8
LeCoupler <p.wotruba@web.de>
2017-04-13 15:32:58 +0200

    technology now plain w/ sanitizer

--- laxar-html-display-widget.js
+++ laxar-html-display-widget.js
@@ -1,45 +1,18 @@
 /**
- * Copyright 2015 aixigo AG
- * Released under the MIT license.
- * http://laxarjs.org/license
+ * Copyright 2017
  */
-define( [
-   'angular',
-   'laxar',
-   'laxar-patterns',
-   'angular-sanitize'
-], function( ng, ax, patterns ) {
-   'use strict';
-
-   var moduleName = 'axHtmlDisplayWidget';
-   var module     = ng.module( moduleName, [ 'ngSanitize' ] );
-
-   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-   Controller.$inject = [ '$scope', 'axI18n' ];
-
-   function Controller( $scope, i18n ) {
-
-      $scope.i18n = i18n;
-
-      $scope.model = { i18nHtmlContent: '' };
-      $scope.resources = {};
-
-      patterns.resources.handlerFor( $scope ).registerResourceFromFeature( 'content', {
-         onUpdateReplace: function( event ) {
-            console.log($scope.model.i18nHtmlContent);
-            $scope.model.i18nHtmlContent =
-               ax.object.path( $scope.resources.content, $scope.features.content.attribute );
-            console.log($scope.model.i18nHtmlContent);
-         }
-      } );
-
-   }
-
-   module.controller( 'AxHtmlDisplayWidgetController', Controller );
-
-   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-   return module;
-
-} );
+import * as ax from 'laxar';
+export const injections = [ 'axWithDom', 'axFeatures', 'axEventBus' ];
+export function create( axWithDom, features, eventBus ) {
+   let htmlText;
+   eventBus.subscribe( `didReplace.${features.content.resource}`, event => {
+      htmlText = ax.object.path( event.data, features.content.attribute );
+   } );
+   return {
+      onDomAvailable() {
+         axWithDom( element => {
+            element.querySelector( 'div' ).innerHTML = htmlText;
+         } );
+      }
+   };
+}
